eba345782f2a253cc3084501b100f8001bc8b46e
wadhah naggui <wadhah.naggui@esprit.tn>
2023-12-09 14:58:38 +0100

    first Service

---
+++ CoachService/src/test/java/com/example/coachservice/CoachServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coachservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
